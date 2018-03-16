a6e788a5a5218c7300661033801c66362b975131
Kenny Koch <kennykoch47@gmail.com>
2018-03-16 16:35:01 -0400

    added semantic ui

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar'
 
+
 export default class Main extends Component {
   render () {
     return (
--- browser/react/components/Navbar.js
+++ browser/react/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Menu } from 'semantic-ui-react'
 import '../../../public/styles.css'
+
 const Navbar = (props) => {
   return (
-    <div className='navbar'>
-      <ul>
-        <li>Home</li>
-        <li>Blog</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
-      </ul>
+    <div>
+    <Menu stackable fluid widths={8} fixed="top" pointing secondary>
+      <Menu.Item active>Home</Menu.Item>
+      <Menu.Item>About</Menu.Item>
+      <Menu.Item>Portfolio</Menu.Item>
+      <Menu.Item>Contact</Menu.Item>
+    </Menu>
     </div>
   );
 };
