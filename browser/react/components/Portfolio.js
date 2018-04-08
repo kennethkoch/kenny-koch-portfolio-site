4c0b27e895a23ee5ba925c2bd4bf25982571ce6e
Kenny Koch <kennykoch47@gmail.com>
2018-04-08 14:21:52 -0400

    added cards to portfolio page

--- browser/react/components/Portfolio.js
+++ browser/react/components/Portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Segment } from 'semantic-ui-react'
+import { Segment, Card } from 'semantic-ui-react'
 import '../../../public/styles.css'
 
 const Portfolio = (props) => {
@@ -9,7 +9,58 @@ const Portfolio = (props) => {
   inverted
   textAlign='center'
   vertical
-  ></Segment>
+  >
+  <Card.Group id='projects' centered itemsPerRow={3}>
+    <Card color='red'>
+    <Card.Content>
+      <Card.Header>Project Title</Card.Header>
+      <Card.Meta>role, time worked on</Card.Meta>
+      <Card.Description>description of the project</Card.Description>
+    </Card.Content>
+    <Card.Content extra>technologies used</Card.Content>
+    </Card>
+    <Card color='orange'>
+    <Card.Content>
+      <Card.Header>Project Title</Card.Header>
+      <Card.Meta>role, time worked on</Card.Meta>
+      <Card.Description>description of the project</Card.Description>
+    </Card.Content>
+    <Card.Content extra>technologies used</Card.Content>
+    </Card>
+    <Card color='yellow'>
+    <Card.Content>
+      <Card.Header>Project Title</Card.Header>
+      <Card.Meta>role, time worked on</Card.Meta>
+      <Card.Description>description of the project</Card.Description>
+    </Card.Content>
+    <Card.Content extra>technologies used</Card.Content>
+    </Card>
+    <Card color='red'>
+    <Card.Content>
+      <Card.Header>Project Title</Card.Header>
+      <Card.Meta>role, time worked on</Card.Meta>
+      <Card.Description>description of the project</Card.Description>
+    </Card.Content>
+    <Card.Content extra>technologies used</Card.Content>
+    </Card>
+    <Card color='orange'>
+    <Card.Content>
+      <Card.Header>Project Title</Card.Header>
+      <Card.Meta>role, time worked on</Card.Meta>
+      <Card.Description>description of the project</Card.Description>
+    </Card.Content>
+    <Card.Content extra>technologies used</Card.Content>
+    </Card>
+    <Card color='yellow'>
+    <Card.Content>
+      <Card.Header>Project Title</Card.Header>
+      <Card.Meta>role, time worked on</Card.Meta>
+      <Card.Description>description of the project</Card.Description>
+    </Card.Content>
+    <Card.Content extra>technologies used</Card.Content>
+    </Card>
+  </Card.Group>
+  </Segment>
     )
 };
 
