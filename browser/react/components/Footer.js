4243ad8f3c04b75e3fb9536753dc18b840cafbf8
Kenny Koch <kennykoch47@gmail.com>
2018-04-17 21:04:58 -0400

    refactored/cleaned up footer

--- browser/react/components/Footer.js
+++ browser/react/components/Footer.js
@@ -3,43 +3,42 @@ import { Segment, Icon, Grid } from 'semantic-ui-react';
 import '../../../public/styles.css';
 
 const Footer = props => (
-  <Segment id="footer" inverted textAlign="center" vertical>
-    {' '}
+  <Segment id='footer' inverted textAlign='center' vertical>
     <Grid
-      padded="horizontally"
-      verticalAlign="middle"
+      padded='horizontally'
+      verticalAlign='middle'
       centered
-      textAlign="centered"
-      columns="equal"
+      textAlign='centered'
+      columns='equal'
     >
       <Grid.Row centered>
-        <Grid.Column textAlign="centered">
+        <Grid.Column textAlign='centered'>
           <div>
             <a href="https://github.com/kennethkoch">
-              <Icon name="github" size="big" />
-                View my GitHub
+              <Icon name='github' size='big' />
+              View my GitHub
             </a>
           </div>
         </Grid.Column>
-        <Grid.Column textAlign="centered">
+        <Grid.Column textAlign='centered'>
           <div>
             <a href="https://www.linkedin.com/in/kenny-koch/">
-              <Icon name="linkedin square" size="big" />
-                Connect on LinkedIn
+              <Icon name='linkedin square' size='big' />
+              Connect on LinkedIn
             </a>
           </div>
         </Grid.Column>
-        <Grid.Column textAlign="centered">
+        <Grid.Column textAlign='centered'>
           <div>
-            <Icon name="mail" size="big" />
-              kennykoch47@gmail.com
+            <Icon name='mail' size='big' />
+            kennykoch47@gmail.com
           </div>
         </Grid.Column>
-        <Grid.Column textAlign="centered">
+        <Grid.Column textAlign='centered'>
           <div>
             <a href="https://twitter.com/kennykoch47">
-              <Icon name="twitter" size="big" />
-                @kennykoch47
+              <Icon name='twitter' size='big' />
+              @kennykoch47
             </a>
           </div>
         </Grid.Column>
