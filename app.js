03577cef8527a529934c9124bf16b7e86d637c46
Kenny Koch <kennykoch47@gmail.com>
2018-04-21 13:11:06 -0400

    fixed process.env.PORT bug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const server = require('./server');
 
-const PORT = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
+server.listen(port, () => console.log(`Listening on Port ${port}`));
