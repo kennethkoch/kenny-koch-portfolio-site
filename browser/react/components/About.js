8f69cd67b9986fd07828207b34b5b186b962c0a5
Kenny Koch <kennykoch47@gmail.com>
2018-04-22 12:00:27 -0400

    made about section more responsive

--- browser/react/components/About.js
+++ browser/react/components/About.js
@@ -4,53 +4,47 @@ import '../../../public/styles.css';
 
 const About = props => (
   <Segment id='about' inverted textAlign='center' vertical>
-    <Grid id='about-grid' columns={2}>
-      <Grid.Row>
-        <Grid.Column>
+    <Grid id='about-grid' centered>
+      <Grid.Row centered>
           <Header as='h1' inverted>
             Who am I?
           </Header>
-        </Grid.Column>
-        <Grid.Column>
+          <p>
+          My name is Kenny Koch and I am a fullstack software engineer. I have experience with
+          both front-end and back-end web development. In February of 2018 I graduated from
+          Fullstack Academy, an immersive coding bootcamp where I spent three months working with
+          some of the smartest people and most talented programmers I've ever met. I'm now focused
+          on continuing to build web and mobile applications and expanding my knowledge. One area
+          which I have become deeply interested in is blockchain technology. <a href="">Click here
+          to check out my blog</a> where I write about the crypto space, and feel free to contact
+          me via any of the methods below!
+          </p>
+          </Grid.Row>
+        <Grid.Row centered>
+        <div>
           <Header as='h1' inverted>
             Skills
           </Header>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column>
-          <p>
-            My name is Kenny Koch and I am a fullstack software engineer. I have experience with
-            both front-end and back-end web development. In February of 2018 I graduated from
-            Fullstack Academy, an immersive coding bootcamp where I spent three months working with
-            some of the smartest people and most talented programmers I've ever met. I'm now focused
-            on continuing to build web and mobile applications and expanding my knowledge. One area
-            which I have become deeply interested in is blockchain technology. <a href="">Click here
-            to check out my blog</a> where I write about the crypto space, and feel free to contact
-            me via any of the methods below!
-          </p>
-        </Grid.Column>
-        <Grid.Column>
-          <List animated divided inverted size='large'>
-            <List.Item>JavaScript</List.Item>
-            <List.Item>HTML/CSS</List.Item>
-            <List.Item>React</List.Item>
-            <List.Item>Redux</List.Item>
-            <List.Item>Node.js</List.Item>
-            <List.Item>Express.js</List.Item>
-            <List.Item>Sequelize</List.Item>
-            <List.Item>React Native</List.Item>
-            <List.Item>Web Design</List.Item>
-            <List.Item>WordPress</List.Item>
-            <List.Item>Bootstrap</List.Item>
-            <List.Item>Semantic UI</List.Item>
-            <List.Item>Socket.io</List.Item>
-            <List.Item>OAuth</List.Item>
-            <List.Item>App Deployment</List.Item>
-            <List.Item>Server Configuration</List.Item>
+          <List animated divided relaxed horizontal inverted size='large'>
+          <List.Item>JavaScript</List.Item>
+          <List.Item>HTML/CSS</List.Item>
+          <List.Item>React</List.Item>
+          <List.Item>Redux</List.Item>
+          <List.Item>Node.js</List.Item>
+          <List.Item>Express.js</List.Item>
+          <List.Item>Sequelize</List.Item>
+          <List.Item>React Native</List.Item>
+          <List.Item>Web Design</List.Item>
+          <List.Item>WordPress</List.Item>
+          <List.Item>Bootstrap</List.Item>
+          <List.Item>Semantic UI</List.Item>
+          <List.Item>Socket.io</List.Item>
+          <List.Item>OAuth</List.Item>
+          <List.Item>App Deployment</List.Item>
+          <List.Item>Server Configuration</List.Item>
           </List>
-        </Grid.Column>
-      </Grid.Row>
+          </div>
+        </Grid.Row>
     </Grid>
   </Segment>
 );
