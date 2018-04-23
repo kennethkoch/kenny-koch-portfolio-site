47774c117291ddf743bd57333e12f0105aded8e6
Kenny Koch <kennykoch47@gmail.com>
2018-04-23 07:48:15 -0400

    added breakpoint, made footer more responsive

--- browser/react/components/Footer.js
+++ browser/react/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = props => (
           <div>
             <a href="https://github.com/kennethkoch">
               <Icon name='github' size='big' />
-              View my GitHub
+              <span className='footer-link-text'>View my GitHub</span>
             </a>
           </div>
         </Grid.Column>
@@ -22,7 +22,7 @@ const Footer = props => (
           <div>
             <a href="https://www.linkedin.com/in/kenny-koch">
               <Icon name='linkedin square' size='big' />
-              Connect on LinkedIn
+              <span className='footer-link-text'>Connect on LinkedIn</span>
             </a>
           </div>
         </Grid.Column>
@@ -30,7 +30,7 @@ const Footer = props => (
           <div>
             <a href="mailto:kennykoch47@gmail.com">
               <Icon name='mail' size='big' />
-              kennykoch47@gmail.com
+              <span className='footer-link-text'>kennykoch47@gmail.com</span>
             </a>
           </div>
         </Grid.Column>
@@ -38,7 +38,7 @@ const Footer = props => (
           <div>
             <a href="https://twitter.com/kennykoch47">
               <Icon name='twitter' size='big' />
-              @kennykoch47
+              <span className='footer-link-text'>@kennykoch47</span>
             </a>
           </div>
         </Grid.Column>
